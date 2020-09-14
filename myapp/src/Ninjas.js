6d929c05aca791fbe85b2dcaa5676243183c2f3b
Shitij Khanna <shitij.khanna@audax.nl>
2020-09-14 18:09:52 +0200

    conditional statements and forms revisited

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -2,14 +2,28 @@ import React from 'react';
 
 const Ninjas = (props) => {
         const {ninjas} = props;
+        // const ninjaList = ninjas.map(ninja => {
+        //     if(ninja.age > 27){
+        //     return (
+        //         <div className = "ninja" key={ninja.id}>
+        //         <div>Name : {ninja.name}</div>
+        //         <div>Age  :  {ninja.age}</div>
+        //         <div>Belt : {ninja.belt}</div>
+        //     </div>
+        //     )
+        // }else{
+        //     return null
+        // }
+        // })
+
         const ninjaList = ninjas.map(ninja => {
-            return (
+            return ninja.age > 27 ? (
                 <div className = "ninja" key={ninja.id}>
-                <div>Name : {ninja.name}</div>
-                <div>Age  :  {ninja.age}</div>
-                <div>Belt : {ninja.belt}</div>
-            </div>
-            )
+                         <div>Name : {ninja.name}</div>
+                         <div>Age  :  {ninja.age}</div>
+                         <div>Belt : {ninja.belt}</div>
+                </div>        
+            ) : null;
         })
         return(
          <div className = "ninja-list">
