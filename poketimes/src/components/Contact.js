ad22668ee1e964ed66c678bd0bd72a915c7abd64
Shitij Khanna <shitij.khanna@audax.nl>
2020-09-21 17:48:19 +0200

    axios and route params

--- poketimes/src/components/Contact.js
+++ poketimes/src/components/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = (props) => {
 
-  console.log(props) 
+  // console.log(props) 
 //   setTimeout(()=>{
 //       props.history.push('/about')
 //   }, 2000);
--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -3,10 +3,10 @@
 
  const Navbar = (props) => {
 
-       setTimeout(()=>{
-              props.history.push('/about')
-          }, 2000);
-    console.log(props)
+      //  setTimeout(()=>{
+      //         props.history.push('/about')
+      //     }, 2000);
+    // console.log(props)
 
     return (
       <nav className="nav-wrapper red darken-3">
